6822bd7e031d97cd594eda8df0f94eef4168fe6d
buttersk <buttersk@gmail.com>
2020-02-16 15:55:35 -0600

    Adding Pojo. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by Ken Butters on 2/16/2020
+ */
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by Ken Butters on 2/16/2020
+ */
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
